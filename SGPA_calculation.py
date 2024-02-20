14d2e6abd0963463d64669117227ccb864c429da
Soumadip Santra <153194426+Soumadip-cmd@users.noreply.github.com>
2024-02-21 01:57:53 +0530

    Update SGPA_calculation.py

--- SGPA_calculation.py
+++ SGPA_calculation.py
@@ -9,15 +9,16 @@ s=0
 
 for i in range(n):
     sub=(input("\n\tEnter Subject Name: "))
-    credit_score=int(input("\n\tEnter Credit-point: "))
+    credit_score=float(input("\n\tEnter Credit-point: "))
     num=int(input("\n\tHow Much Point You Got(sgpa-format(5-10)): "))
+    
     sub_l.append(sub)
     credit_l.append(credit_score)
     num_l.append(num)
     s+=(credit_score*num)
     total+=(credit_score*10)
 for j in range(n):
-    print("\n\n\t{}:- (credit-score)-({}) You got:{}".format(sub_l[j],credit_l[j],num_l[j]))
+    print("\n\t{}:- (credit-score)-({}) You got:{}".format(sub_l[j].capitalize(),credit_l[j],num_l[j]))
 
 sgpa=0
 sgpa=(s/total)*10
