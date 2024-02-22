5a67d7b023eb4e0addea6fa2276bfa8c17be6fe5
Soumadip Santra <153194426+Soumadip-cmd@users.noreply.github.com>
2024-02-22 18:54:31 +0530

    Update SGPA_calculation.py

--- SGPA_calculation.py
+++ SGPA_calculation.py
@@ -6,20 +6,31 @@ credit_l=[]
 num_l=[]
 total=0
 s=0
+want=(input("\n\tDo You want Shortcut Calculation:[Enter 'y' else 'n']: "))
 
 for i in range(n):
-    sub=(input("\n\tEnter Subject Name: "))
-    credit_score=float(input("\n\tEnter Credit-point: "))
-    num=int(input("\n\tHow Much Point You Got(sgpa-format(5-10)): "))
-    
-    sub_l.append(sub)
-    credit_l.append(credit_score)
-    num_l.append(num)
-    s+=(credit_score*num)
-    total+=(credit_score*10)
-for j in range(n):
-    print("\n\t{}:- (credit-score)-({}) You got:{}".format(sub_l[j].capitalize(),credit_l[j],num_l[j]))
-
+    if want=='y':
+        credit_score=float(input("\n\tEnter Credit-point: "))
+        num=int(input("\n\tHow Much Point You Got(sgpa-format(5-10)): "))
+        credit_l.append(credit_score)
+        num_l.append(num)
+        s+=(credit_score*num)
+        total+=(credit_score*10)
+    else:
+        sub=(input("\n\tEnter Subject Name: "))
+        credit_score=float(input("\n\tEnter Credit-point: "))
+        num=int(input("\n\tHow Much Point You Got(sgpa-format(5-10)): "))
+        sub_l.append(sub)
+        credit_l.append(credit_score)
+        num_l.append(num)
+        s+=(credit_score*num)
+        total+=(credit_score*10)
+if want=='y':
+    for j in range(n):
+        print("\n\t(credit-score)-({}) You got:{}".format(credit_l[j],num_l[j]))
+else:
+    for j in range(n):
+        print("\n\t{}:- (credit-score)-({}) You got:{}".format(sub_l[j].capitalize(),credit_l[j],num_l[j]))
 sgpa=0
 sgpa=(s/total)*10
 print('\n\tYour Sgpa is- ',sgpa)
